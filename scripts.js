a3188e66409e3c7e133db3cbb870cae6b0b21c8a
wenF7752 <113209938+wenF7752@users.noreply.github.com>
2024-09-23 16:10:43 -0400

    part 2 finished

--- scripts.js
+++ scripts.js
@@ -46,3 +46,46 @@ function countUp(n) {
 }
 
 countUp(5);
+
+//Part 2: Thinking Methodically
+// When functions are built into objects, like Arrays, they are referred to as “methods” of those objects. Many methods, including Array methods, require “callback functions” to determine their behavior.
+// For the tasks below, use the following data to test your work:
+// [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" }, { id: "48", name: "Barry", occupation: "Runner", age: "25" }, { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" }, { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" }, { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
+// Use callback functions alongside Array methods to accomplish the following:
+// Sort the array by age.
+// Filter the array to remove entries with an age greater than 50.
+// Map the array to change the “occupation” key to “job” and increment every age by 1.
+// Use the reduce method to calculate the sum of the ages.
+// Then use the result to calculate the average age.
+
+const people = [];
+people.push({ id: "42", name: "Bruce", occupation: "Knight", age: "41" });
+people.push({ id: "48", name: "Barry", occupation: "Runner", age: "25" });
+people.push({ id: "57", name: "Bob", occupation: "Fry Cook", age: "19" });
+people.push({ id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" });
+people.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
+
+// Sort the array by age.
+people.sort((a, b) => b.age - a.age);
+console.log(people);
+// Filter the array to remove entries with an age greater than 50.
+const under50 = people.filter((person) => person.age <= 50);
+console.log(under50);
+
+// Map the array to change the “occupation” key to “job” and increment every age by 1.
+const newPeople = people.map((person) => {
+  return {
+    id: person.id,
+    name: person.name,
+    job: person.occupation,
+    age: parseInt(person.age) + 1,
+  };
+});
+console.log(newPeople);
+
+// Use the reduce method to calculate the sum of the ages.
+const totalAge = people.reduce(
+  (total, person) => total + parseInt(person.age),
+  0
+);
+console.log(totalAge);
